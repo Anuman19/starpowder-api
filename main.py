915121b537cf0979bdb655565603de9f07262073
Ivan <cammi@bbcag.ch>
2022-06-15 11:04:17 +0200

    working draft

--- main.py
+++ main.py
@@ -6,20 +6,39 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 
-class Temp(BaseModel):
-    type: str
-    data: str
+temp = {
+    "celsius":"0",
+    "pressure":"0",
+    "humidity":"0"}
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-@app.post("/temp/")
-async def post_temp(temp: Temp):
     return temp
 
+@app.post("/temp")
+async def post_temp(newTemp: str):
+    temp["celsius"] = newTemp
 
+@app.get("/temp")
+async def get_temp():
+    return temp
+
+@app.post("/hum")
+async def post_hum(newHum: str):
+    temp["humidity"] = newHum
+
+@app.get("/hum")
+async def get_hum():
+    return temp["humidity"]
+
+@app.post("/pressure")
+async def post_pressure(newPressure: str):
+    temp["pressure"] = newPressure
+
+@app.get("/pressure")
+async def get_pressure():
+    return temp["pressure"]
 
