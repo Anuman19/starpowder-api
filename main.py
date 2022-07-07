9e6c411a8db4928b0be567b341ad62bfaef3f5cd
Anpalagan Naveen <naveen.anpalagan@bbcag.ch>
2022-07-07 09:19:38 +0200

    ip adresses

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from pydantic import BaseModel
 import requests
 
 temp = {
-    "celsius":"0",
-    "pressure":"0",
-    "humidity":"0"}
+    "celsius": "0",
+    "pressure": "0",
+    "humidity": "0"}
 
 app = FastAPI()
 
@@ -18,27 +18,32 @@ app = FastAPI()
 async def root():
     return temp
 
+
 @app.post("/temp")
 async def post_temp(newTemp: str):
     temp["celsius"] = newTemp
+
 
 @app.get("/temp")
 async def get_temp():
     return temp
 
+
 @app.post("/hum")
 async def post_hum(newHum: str):
     temp["humidity"] = newHum
+
 
 @app.get("/hum")
 async def get_hum():
     return temp["humidity"]
 
+
 @app.post("/pressure")
 async def post_pressure(newPressure: str):
     temp["pressure"] = newPressure
 
+
 @app.get("/pressure")
 async def get_pressure():
     return temp["pressure"]
-
--- mqtt.py
+++ mqtt.py
@@ -3,10 +3,11 @@ import requests
 import json
 import datetime
 
+
 def on_connect(client, userdata, flags, rc):
     client.subscribe("rooms/shedhalle", qos=0)
-    client.subscribe("rooms/shedahlle/humidity", qos=0)
-    
+    client.subscribe("rooms/shedhalle/humidity", qos=0)
+
 
 def on_message(client, userdata, msg):
     print(msg.payload.decode())
@@ -14,31 +15,32 @@ def on_message(client, userdata, msg):
     msg_json = json.loads(msg_decoded)
     timestamp = datetime.datetime.utcnow()
     body1 = {
-            "measurement": "temperature",
-            "time": str(timestamp),
-            "celsius": float(msg_json["temperature"])
-        }
-   
-    
+        "measurement": "temperature",
+        "time": str(timestamp),
+        "celsius": float(msg_json["temperature"])
+    }
+
     # INFLUXDB_CLIENT.write_points(body)
     print(msg_json["pressure"])
-    responseTemp = requests.post("http://127.0.0.1:8000/temp?newTemp="+str(msg_json["temperature"]), data={"test":"test"})
-    responsePressure = requests.post("http://127.0.0.1:8000/pressure?newPressure="+str(msg_json["pressure"]), data={"test":"test"})
-    responseHum = requests.post("http://127.0.0.1:8000/hum?newHum="+str(msg_json["humidity"]), data={"test":"test"})
+    responseTemp = requests.post("http://127.0.0.1:8000/temp?newTemp=" + str(msg_json["temperature"]),
+                                 data={"test": "test"})
+    responsePressure = requests.post("http://127.0.0.1:8000/pressure?newPressure=" + str(msg_json["pressure"]),
+                                     data={"test": "test"})
+    responseHum = requests.post("http://127.0.0.1:8000/hum?newHum=" + str(msg_json["humidity"]), data={"test": "test"})
 
     print(responseTemp)
     print(responsePressure)
     print(responseHum)
-    #requests.get('http://127.0.0.1:8000/')
+    # requests.get('http://127.0.0.1:8000/')
 
 
 MQTT_IP_ADDRESS = '127.0.0.1'
-MQTT_USER = "ioeuser" 
+MQTT_USER = "ioeuser"
 MQTT_PASSWORD = "2020"
 MQTT_CLIENT = mqtt.Client("MQTT_PYTHON")
 MQTT_CLIENT.on_connect = on_connect
-#MQTT_CLIENT.on_message = on_message
-MQTT_CLIENT.message_callback_add("rooms/shedhalle",on_message)
+# MQTT_CLIENT.on_message = on_message
+MQTT_CLIENT.message_callback_add("rooms/shedhalle", on_message)
 MQTT_CLIENT.username_pw_set(username=MQTT_USER, password=MQTT_PASSWORD)
 MQTT_CLIENT.connect(MQTT_IP_ADDRESS, 1883, 60)
 MQTT_CLIENT.loop_forever()
